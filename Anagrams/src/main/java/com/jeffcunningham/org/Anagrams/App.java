171c0d09ba8bf45b905594733fb9eeb3df908c6c
Jeff Cunningham <jcunningham77@gmail.com>
2018-03-16 19:33:15 -0400

    anagrams solution

--- Anagrams/src/main/java/com/jeffcunningham/org/Anagrams/App.java
+++ Anagrams/src/main/java/com/jeffcunningham/org/Anagrams/App.java
@@ -1,7 +1,15 @@
 package com.jeffcunningham.org.Anagrams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
 
 /**
  * Hello world!
@@ -11,13 +19,12 @@ public class App
 {
 	private static int counter = 0; 
 	
-    public static int numberNeeded(String first, String second) {
+//	private static final Logger logger = LogManager.getLogger(App.class);
+	
+	
+    private static int numberNeeded(String first, String second) {
     		
-//	    	char[] firstChars = first.toCharArray();
-//	    	char[] secondChars = first.toCharArray();
-//	    	
-//	    	Arrays.sort(firstChars);
-//	    	Arrays.sort(secondChars);
+
 	    	
 	    	int characterCountMatrix[][] = new int[26][2];
 	    
@@ -53,20 +60,65 @@ public class App
 	    
         characterCountMatrixLetterIndex=0; 
         for(c = 'a'; c <= 'z'; ++c) {
-        		       	
-        		System.out.println("# of occurences of " + c + " In first string: " + characterCountMatrix[characterCountMatrixLetterIndex][0] + " In second string: " + characterCountMatrix[characterCountMatrixLetterIndex][1]);
+//        	logger.info("# of occurences of " + c + " In first string: " + characterCountMatrix[characterCountMatrixLetterIndex][0] + " In second string: " + characterCountMatrix[characterCountMatrixLetterIndex][1]);
         		characterCountMatrixLetterIndex++;
         }
+        characterCountMatrixLetterIndex=0; 
+        
+        
+        for(c = 'a'; c <= 'z'; ++c) {
+	      
+        	
+			if (characterCountMatrix[characterCountMatrixLetterIndex][0]>characterCountMatrix[characterCountMatrixLetterIndex][1]) {
+//				logger.info("The first string has " + characterCountMatrix[characterCountMatrixLetterIndex][0] + " occurences of " + c + " and the second string has only " + characterCountMatrix[characterCountMatrixLetterIndex][1] + " occurences of  " + c );
+				
+//				logger.info("So removing " + (characterCountMatrix[characterCountMatrixLetterIndex][0] - characterCountMatrix[characterCountMatrixLetterIndex][1]) + " occurences of " + c + " from first string");
+				first = removeOccurrencesOfCharacter(c,first,(characterCountMatrix[characterCountMatrixLetterIndex][0] - characterCountMatrix[characterCountMatrixLetterIndex][1]));	
+			}
+			if (characterCountMatrix[characterCountMatrixLetterIndex][1]>characterCountMatrix[characterCountMatrixLetterIndex][0]) {
+//				logger.info("The second string has " + characterCountMatrix[characterCountMatrixLetterIndex][1] + " occurences of " + c + " and the first string has only " + characterCountMatrix[characterCountMatrixLetterIndex][0] + " occurences of  " + c );
+				
+//				logger.info("So removing " + (characterCountMatrix[characterCountMatrixLetterIndex][1] - characterCountMatrix[characterCountMatrixLetterIndex][0]) + " occurences of " + c + " from second string");
+				first = removeOccurrencesOfCharacter(c,second,(characterCountMatrix[characterCountMatrixLetterIndex][1] - characterCountMatrix[characterCountMatrixLetterIndex][0]));	
+			}			
+			characterCountMatrixLetterIndex++; 
+        }
+        
+        
+        char[] firstCharArray = first.toCharArray();
+        Arrays.sort(firstCharArray);
+        String sortedFirst = new String(firstCharArray);
+//        logger.info("Sorted and trimmed first string = " + sortedFirst);  
+        
+        char[] secondCharArray = second.toCharArray();
+        Arrays.sort(secondCharArray);
+        String sortedSecond = new String(secondCharArray);
+//        logger.info("Sorted and trimmed second string = " + sortedSecond);          
 	        
 	        
     	
         return counter;
+    }
+    
+    private static String removeOccurrencesOfCharacter(char c, String inputString, int numberOfOccurences) {
+    		
+    		
+    		for (int i = 0; i < numberOfOccurences; i++){
+    			inputString =  inputString.replaceFirst(String.valueOf(c), "");
+//    			logger.info("removeOccurrencesOfCharacter: Just removed an occurence of " + c + ", resulting in " + inputString);
+    			counter++;
+    		}
+//    		logger.info("returning resulting string:  " + inputString);
+    		return inputString;
+    		
     }
   
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String a = in.next();
         String b = in.next();
-        System.out.println(numberNeeded(a, b));
+        
+//        logger.info(numberNeeded(a, b) + " total characters were removed");
+        System.out.println(numberNeeded(a, b) + " total characters were removed");
     }
 }
